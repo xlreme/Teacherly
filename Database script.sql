2e83d7a497e3536af17b3ed05ebbe1f3cc909ba6
xlreme <zoro3xd@hotmail.com>
2019-03-21 22:10:38 +0800

    Update Database script

--- Database script.sql
+++ Database script.sql
@@ -6,11 +6,16 @@ CREATE TABLE IF NOT EXISTS `teacherly`.`teachersandstudents` (
   `TeacherEmail` VARCHAR(500) CHARACTER SET 'latin1' NOT NULL,
   `StudentEmail` VARCHAR(500) CHARACTER SET 'latin1' NOT NULL,
   `CreatedDt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `ModifiedDt` DATETIME NULL DEFAULT ON UPDATE CURRENT_TIMESTAMP,
+  `ModifiedDt` DATETIME NULL DEFAULT NULL,
   `Suspended` BIT(1) NULL DEFAULT b'0',
   PRIMARY KEY (`TeacherEmail`, `StudentEmail`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1
+
+# 3. Set ModifiedDT as on update
+ALTER TABLE `teacherly`.`teachersandstudents` 
+CHANGE COLUMN `ModifiedDt` `ModifiedDt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ;
+
 
 # (Optional) Give root user legacy access using password if require
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password3'
